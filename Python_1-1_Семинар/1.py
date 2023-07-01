67d74161445bb4e7cdd0e3a4e2332f3010623a30
LeandraLi <leandra2005@yandex.ru>
2023-07-01 17:58:47 +0300

    1st commit of Python repository

---
+++ Python_1-1_Семинар/1.py
@@ -0,0 +1,13 @@
+#Ввести два числа и определить наибольшее
+
+x = int(input('Введите число X - '))
+y = int(input('Введите число Y - '))
+
+if x > y:
+    print('x>y')
+elif x == y:
+    print('x=y')
+if y > x:
+    print('y>x')
+
+print(x==y)

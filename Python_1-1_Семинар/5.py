51678b73d48a00819a7a0a07472b4e249e308ef0
LeandraLi <leandra2005@yandex.ru>
2023-07-02 09:30:57 +0300

    Commit S_1_1_Task 6

--- Python_1-1_Семинар/5.py
+++ Python_1-1_Семинар/5.py
@@ -6,6 +6,6 @@
 
 year = int(input('Введите год: '))
 if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
-    print('Это високосный год')
+    print('YES')
 else:
-    print('Это не високосный год')
+    print('NO')

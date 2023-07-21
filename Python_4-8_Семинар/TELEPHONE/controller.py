758d517ec3f264c26ace436936ba59d6d5053e68
LeandraLi <leandra2005@yandex.ru>
2023-07-21 17:37:07 +0400

    Commit HW-8: adding HW

--- Python_4-8_Семинар/TELEPHONE/controller.py
+++ Python_4-8_Семинар/TELEPHONE/controller.py
@@ -10,7 +10,22 @@ def main():
         elif num == 2:
             char = input_char()
             find_info(char)
+        elif num == 3:
+            char = input_char()
+            lst_sel = find_info(char)
+            sel_num = select_num()
+            info = input_info()
+            # теперь нужная строка для изменения хранится в следующем списке: lst_sel[sel_num-1]
+        elif num == 4:
+            char = input_char()
+            find_info(char)
+            sel_num = select_num()
         elif num == 5:
+            char = input_char()
+            find_info(char)
+        elif num == 6:
+            view_all()
+        elif num == 7:
             print("Выход из программы") # сразу пишем сообщение, т.к. знаем сразу, что 5 - это выход
             pass
             break
--- Python_4-8_Семинар/TELEPHONE/database.py
+++ Python_4-8_Семинар/TELEPHONE/database.py
@@ -4,11 +4,29 @@ def write_info(info):
 
 def find_info(char):
     with open("data.txt","r", encoding = "utf-8") as file:
+        lst_sel = []
         lst = file.readlines() # по сути lst - это наши работники (элементы списка)
         count = 0
         for line in lst: # и запускаем цикл по этому списку
-            if char in line:
+            if char in line: # если искомая характеристика есть в строке, то присваиваем ей номер по порядку
                 count += 1
                 print(f"{count}) {line.strip()}") # метод strip убирает лишнюю строку между строками при выводе данных
+                lst_sel.append(line)
+            return lst_sel
+
+def view_all():
+    with open("data.txt","r", encoding = "utf-8") as file:
+        print(file.read())
+
+def change_data(old_data, new_data): # перезапишем наш файлик с измененной строчкой
+    with open("data.txt","r", encoding = "utf-8") as file:
+        lst_old = file.readlines()
+    with open("data.txt","w", encoding = "utf-8") as file:
+        for line in lst_old:
+            if old_data in line:
+                file.write(new_data)
+            else:
+                file.write(line)
+
 
 
